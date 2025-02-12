3744667fbda4fd10a660101bb394d3620be7cc09
sindhu110 <ammulusindhu42@gmail.com>
2025-02-12 08:20:02 +0000

    updated

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 document.querySelectorAll('nav ul li a').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        const targetId = this.getAttribute('href').substring(1);
-        const targetSection = document.getElementById(targetId);
-        targetSection.scrollIntoView({ behavior: 'smooth' });
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+      const targetId = this.getAttribute('href').substring(1);
+      const targetSection = document.getElementById(targetId);
+      if (targetSection) {
+        window.scrollTo({
+          top: targetSection.offsetTop - 50, // Adjust this value if needed
+          behavior: 'smooth'
+        });
+      }
     });
-});
+  });
